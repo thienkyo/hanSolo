f1471ee94312fbf5f7c40e0b54503482b92eaef1
thienkyo <thienkyo@gmail.com>
2022-09-03 17:02:01 +0700

    test intial

---
+++ src/main/java/com/hanSolo/kinhNguyen/KinhNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.hanSolo.kinhNguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinhNguyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinhNguyenApplication.class, args);
+	}
+
+}
