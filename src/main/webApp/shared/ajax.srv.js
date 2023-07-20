33dfb65bd244bf695811e59f55fc2920f69aaee7
trongthien.le <trongthien.le@antgroup.com>
2023-07-20 20:38:57 +0700

    update:  big update, UI change

--- src/main/webApp/shared/ajax.srv.js
+++ src/main/webApp/shared/ajax.srv.js
@@ -1,8 +1,8 @@
 angular.module('networkServices',[])
 	.factory('ajaxService',['$http','$cookies',function($http,$cookies){
 		
-		//var urlbase = "http://192.168.1.20:8080/";
 		var urlbase = "http://localhost/"; // dev
+		//var urlbase = "http://128.199.156.9/";
 		//var urlbase = "http://matkinhnguyen.com/";
 		var sessionid = $cookies.get('JSESSIONID');
 		
